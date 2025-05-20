4f3fb2123a88d17cfd32b4e3092b18e871fef4bc
Michael Depetrillo <klassicd@gmail.com>
2025-05-20 15:59:29 -0700

    fix: update privy config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { PrivyProvider } from "@privy-io/react-auth";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { sepolia } from "viem/chains";
 import "./index.css";
 
 // Import the generated route tree
@@ -26,6 +27,19 @@ if (!rootElement.innerHTML) {
       <PrivyProvider
         appId={import.meta.env.VITE_PRIVY_APP_ID}
         clientId={import.meta.env.VITE_PRIVY_CLIENT_ID}
+        config={{
+          // only show these login methods in the modal
+          loginMethods: [
+            "google",
+            "discord",
+            "farcaster",
+            "passkey",
+            "wallet", // will include embedded & external wallets
+          ],
+          // only allow Ethereum
+          supportedChains: [sepolia],
+          defaultChain: sepolia,
+        }}
       >
         <RouterProvider router={router} />
       </PrivyProvider>
